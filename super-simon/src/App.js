c67e22a65a989c6944d2dd46f6037447146da548
Julien <julien.quennehen@edu.devinci.fr>
2019-11-18 00:57:22 +0100

    Début mise en place html/css

--- super-simon/src/App.js
+++ super-simon/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Simon">
+      <h1 className="Title">Super Simon</h1>
+      <h4 className="Check">✔</h4>
+      <h4 className="Vague">Vague :</h4>
+      <button className="Info"></button>
+      <button className="bRouge">Rouge</button>
+      <button className="bBleu">Bleu</button>
+      <button className="bViolet">Violet</button>
+      <button className="bJaune">Jaune</button>
+      <button className="bBleuCiel">Bleu Ciel</button>
+      <button className="bVert">Vert</button>
+      <button>Jouer</button>
+      <button>Rejouer</button>
+      <p>Votre Tour</p>
     </div>
   );
 }
